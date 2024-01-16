c86f7acd0a6de1953cc52b036fedf71808f45358
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-01-16 22:31:06 +0000

    add:filtering car list

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,6 +1,7 @@
 import CarNav from "../../components/Navbar/CarNav";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Manual from "../../../images/9081001_manual_gearbox_icon.png";
 import Automatic from "../../../images/automatic-transmission.png";
 import Seats from "../../../images/9035117_person_icon.png";
@@ -15,118 +16,222 @@ import Electric from "../../../images/4801291_automobile_car_eco_electric_powere
 export default function Cars() {
   const cars = useSelector((state) => state.car.cars);
   const days = useSelector((state) => state.car.day);
+  const [filteredCars, setFilteredCars] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
+
+  const handleFilterByType = (type) => {
+    setSelectedType(type);
+
+    const filteredCars = cars.filter((car) => car.type === type);
+    setFilteredCars(filteredCars);
+  };
 
   return (
     <>
       <CarNav />
       <div className="mt-5 font-Inria-Serif">
         <div className="mt-5 mx-[30px]">
-          <h2 className="font-bold text-2xl">Available Cars : {cars.length}</h2>
+          <h2 className="font-bold text-2xl">
+            Available Cars : {selectedType ? filteredCars.length : cars.length}
+          </h2>
           <div className="mt-5">
             <div className="flex justify-between items-center">
               <div className="flex flex-col justify-center items-center">
-                <img src={Hatchback} />
-                <p>Hatchback</p>
+                <button onClick={() => handleFilterByType("Hatchback")}>
+                  <img src={Hatchback} />
+                  <p>Hatchback</p>
+                </button>
               </div>
               <div className="flex flex-col justify-center items-center">
-                <img src={Sedan} />
-                <p>Sedan</p>
+                <button onClick={() => handleFilterByType("Sedan")}>
+                  <img src={Sedan} />
+                  <p>Sedan</p>
+                </button>
               </div>
               <div className="flex flex-col justify-center items-center">
-                <img src={SUV} />
-                <p>SUV</p>
+                <button onClick={() => handleFilterByType("SUV")}>
+                  <img src={SUV} />
+                  <p>SUV</p>
+                </button>
               </div>
               <div className="flex flex-col justify-center items-center">
-                <img src={Electric} />
-                <p>Electric</p>
+                <button onClick={() => handleFilterByType("Electric")}>
+                  <img src={Electric} />
+                  <p>Electric</p>
+                </button>
               </div>
             </div>
           </div>
           <div className=" mt-5 flex flex-col justify-center items-center gap-5 w-full">
-            {cars.map((car) => (
-              <div
-                key={car.id}
-                className="flex justify-between items-start flex-col"
-              >
-                <div>
-                  <img src={car.images} className="w-[350px] h-[250px]" />
-                </div>
-                <div className="bg-[#ebebeb] w-full rounded -mt-2 p-2">
-                  <h3 className="font-semibold text-xl">{car.carName}</h3>
-                  <div className="grid grid-rows-4 grid-cols-2 gap-2">
-                    <p className="flex gap-2">
-                      {" "}
-                      {car.details.gearbox === "Automatic" ? (
-                        <img
-                          src={Automatic}
-                          alt="automatic"
-                          className="w-[30px] h-[30px]"
-                        />
-                      ) : (
-                        <img
-                          src={Manual}
-                          alt="Manual"
-                          className="w-[30px] h-[30px]"
-                        />
-                      )}
-                      {car.details.gearbox}
-                    </p>
-                    <p className="flex gap-2">
-                      {" "}
-                      <img
-                        src={Seats}
-                        alt="automatic"
-                        className="w-[30px] h-[30px]"
-                      />
-                      {car.details.seats} Seats
-                    </p>
-                    <p className="flex gap-2">
-                      {" "}
-                      <img
-                        src={LargeBag}
-                        alt="automatic"
-                        className="w-[30px] h-[30px]"
-                      />
-                      1 Large Bag
-                    </p>
-                    <p className="flex gap-2">
-                      {" "}
-                      <img
-                        src={SmallBag}
-                        alt="automatic"
-                        className="w-[30px] h-[30px]"
-                      />
-                      1 Small Bag
-                    </p>
-                    <p className="flex gap-2">
-                      {" "}
-                      <img
-                        src={Miles}
-                        alt="automatic"
-                        className="w-[30px] h-[30px]"
-                      />
-                      Unlimited Miles
-                    </p>
+            {selectedType
+              ? filteredCars.map((car) => (
+                  <div
+                    key={car.id}
+                    className="flex justify-between items-start flex-col"
+                  >
+                    <div>
+                      <img src={car.images} className="w-[350px] h-[250px]" />
+                    </div>
+                    <div className="bg-[#ebebeb] w-full rounded -mt-2 p-2">
+                      <h3 className="font-semibold text-xl">{car.carName}</h3>
+                      <div className="grid grid-rows-4 grid-cols-2 gap-2">
+                        <p className="flex gap-2">
+                          {" "}
+                          {car.details.gearbox === "Automatic" ? (
+                            <img
+                              src={Automatic}
+                              alt="automatic"
+                              className="w-[30px] h-[30px]"
+                            />
+                          ) : (
+                            <img
+                              src={Manual}
+                              alt="Manual"
+                              className="w-[30px] h-[30px]"
+                            />
+                          )}
+                          {car.details.gearbox}
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={Seats}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          {car.details.seats} Seats
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={LargeBag}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          1 Large Bag
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={SmallBag}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          1 Small Bag
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={Miles}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          Unlimited Miles
+                        </p>
+                      </div>
+                      <div>
+                        <p className="opacity-70">
+                          Price for{" "}
+                          {days === 1 ? (
+                            <span>{days} day</span>
+                          ) : (
+                            <span>{days} days</span>
+                          )}
+                        </p>
+                        <p className="font-bold">£{car.price.perDay}</p>
+                        <Link key={car.id} to={`/car-urus/cars/${car.id}`}>
+                          <button className="bg-[#0370EF] w-40 h-10 rounded-lg text-white font-bold">
+                            View Deal
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    <p className="opacity-70">
-                      Price for{" "}
-                      {days === 1 ? (
-                        <span>{days} day</span>
-                      ) : (
-                        <span>{days} days</span>
-                      )}
-                    </p>
-                    <p className="font-bold">£{car.price.perDay}</p>
-                    <Link key={car.id} to={`/car-urus/cars/${car.id}`}>
-                      <button className="bg-[#0370EF] w-40 h-10 rounded-lg text-white font-bold">
-                        View Deal
-                      </button>
-                    </Link>
+                ))
+              : cars.map((car) => (
+                  <div
+                    key={car.id}
+                    className="flex justify-between items-start flex-col"
+                  >
+                    <div>
+                      <img src={car.images} className="w-[350px] h-[250px]" />
+                    </div>
+                    <div className="bg-[#ebebeb] w-full rounded -mt-2 p-2">
+                      <h3 className="font-semibold text-xl">{car.carName}</h3>
+                      <div className="grid grid-rows-4 grid-cols-2 gap-2">
+                        <p className="flex gap-2">
+                          {" "}
+                          {car.details.gearbox === "Automatic" ? (
+                            <img
+                              src={Automatic}
+                              alt="automatic"
+                              className="w-[30px] h-[30px]"
+                            />
+                          ) : (
+                            <img
+                              src={Manual}
+                              alt="Manual"
+                              className="w-[30px] h-[30px]"
+                            />
+                          )}
+                          {car.details.gearbox}
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={Seats}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          {car.details.seats} Seats
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={LargeBag}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          1 Large Bag
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={SmallBag}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          1 Small Bag
+                        </p>
+                        <p className="flex gap-2">
+                          {" "}
+                          <img
+                            src={Miles}
+                            alt="automatic"
+                            className="w-[30px] h-[30px]"
+                          />
+                          Unlimited Miles
+                        </p>
+                      </div>
+                      <div>
+                        <p className="opacity-70">
+                          Price for{" "}
+                          {days === 1 ? (
+                            <span>{days} day</span>
+                          ) : (
+                            <span>{days} days</span>
+                          )}
+                        </p>
+                        <p className="font-bold">£{car.price.perDay}</p>
+                        <Link key={car.id} to={`/car-urus/cars/${car.id}`}>
+                          <button className="bg-[#0370EF] w-40 h-10 rounded-lg text-white font-bold">
+                            View Deal
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                ))}
           </div>
         </div>
       </div>
