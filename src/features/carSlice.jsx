ff152046086287a062def254311a86ae710237fe
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-01-25 14:50:21 +0000

    add:small update

--- src/features/carSlice.jsx
+++ src/features/carSlice.jsx
@@ -5,7 +5,7 @@ const getCurrentDate = () => {
   const currentDate = new Date();
 
   const day = String(currentDate.getDate()).padStart(2, "0");
-  const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
   const year = currentDate.getFullYear();
 
   return `${year}-${month}-${day}`;
